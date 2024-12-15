5fd40e844f1cd968dbda0e7e3828e3716bf5a29a
vaausud <15054854614@163.com>
2024-12-15 14:35:50 +0800

    feat: 引入配置文件并重构更新逻辑
    - 新增 `config.json` 和 `config.prod.json` 用于 SSH 配置和路径，增强部署环境的灵活性。
    - 重构 `src/utils/config.ts` 以从新的 JSON 文件中读取 SSH 配置，提高可维护性。
    - 更新 `src/services/update.ts` 以在更新过程中添加更详细的日志记录和错误处理。
    - 在 `src/index.ts` 中引入新的 `showMenu` 函数，以在更新期间提供更好的用户交互。
    - 将 `rollup.config.js` 替换为新的 `tsup.config.ts`，简化构建配置。
    - 将 `tsconfig.json` 中的 TypeScript 目标从 ES2019 更改为 ES2015，以提高兼容性。
    - 移除旧的 `rollup.config.js`，作为构建系统过渡的一部分。
    
    此提交增强了项目的配置管理和更新期间的用户体验。

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,28 +1,22 @@
 import { join } from 'path';
-import { readFileSync } from 'fs';
 import { SSHConfig } from './types.js';
 
-// 读取配置文件
-let config;
-try {
-  const configPath = join(process.cwd(), 'config.json');
-  config = JSON.parse(readFileSync(configPath, 'utf-8'));
-  console.log(configPath);
-} catch (error) {
-  throw new Error(`配置文件读取失败: ${error instanceof Error ? error.message : String(error)}`);
-}
-
-// 检查配置项是否存在
-if (!config.cabinetSSHConfig || !config.serverSSHConfig || !config.paths) {
-  throw new Error('配置文件缺少必要的配置项');
-}
-
 // SSH 配置
-export const cabinetSSHConfig: SSHConfig = config.cabinetSSHConfig;
-export const serverSSHConfig: SSHConfig = config.serverSSHConfig;
+export const cabinetSSHConfig: SSHConfig = {
+  host: "user@192.168.8.2",
+  port: 22,
+  uploadPath: "/home/user/smart-cabinet"
+};
+
+export const serverSSHConfig: SSHConfig = {
+  host: "root@192.168.1.50", 
+  port: 22,
+  uploadPath: "/opt/hjrich/smart-cabinet/cabinet-server-update",
+  targetPath: "/opt/hjrich/cabinet-server"
+};
 
 // 路径配置
-export const backendFolderPath = join(process.cwd(), config.paths.backendFolderPath);
-export const cabinetFolderPath = join(process.cwd(), config.paths.cabinetFolderPath);
-export const doorFolderPath = join(process.cwd(), config.paths.doorFolderPath);
-export const backgroundFolderPath = join(process.cwd(), config.paths.backgroundFolderPath);
+export const backendFolderPath = join(process.cwd(), "后台更新程序");
+export const cabinetFolderPath = join(process.cwd(), "载体柜更新程序");
+export const doorFolderPath = join(process.cwd(), "通道门更新程序");
+export const backgroundFolderPath = join(process.cwd(), "载体柜程序背景图片");
